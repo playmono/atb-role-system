baa76013920c4b20ce4f7d7f2c1a5fdc1447d606
playmono <adrgranado@gmail.com>
2023-05-21 00:55:38 +0200

    chore: tabs to spaces

--- backend/index.js
+++ backend/index.js
@@ -17,75 +17,75 @@ app.use(bodyParser.json());
 app.get("/", (req, res, next) => res.send("Hello world!"));
 
 app.post("/sign-up", (req, res) => {
-	console.log('Received POST /sign-up');
-	const data = req.body;
+    console.log('Received POST /sign-up');
+    const data = req.body;
 
-	Player.validateData(data)
-		.then((data) => database.findPlayerByUsername(data.username))
-		.then((player) => {
-			return new Promise((resolve, reject) => {
-				if (player) {
-					const e = new Error('Username already exists.');
-					e.code = "VALIDATION_ERROR";
-					reject(e);
-				} else {
-					resolve();
-				}
-			})
-		})
-		.then(() => Player.createPlayer({username: data.username, password: data.password}))
-		.then((player) => database.save(player))
-		.then((player) => {
-			console.log(`player ${player.username} saved`);
-			player.hidePassword();
-			res.send(player);
-		})
-		.catch ((error) => {
-			console.log(error);
+    Player.validateData(data)
+        .then((data) => database.findPlayerByUsername(data.username))
+        .then((player) => {
+            return new Promise((resolve, reject) => {
+                if (player) {
+                    const e = new Error('Username already exists.');
+                    e.code = "VALIDATION_ERROR";
+                    reject(e);
+                } else {
+                    resolve();
+                }
+            })
+        })
+        .then(() => Player.createPlayer({username: data.username, password: data.password}))
+        .then((player) => database.save(player))
+        .then((player) => {
+            console.log(`player ${player.username} saved`);
+            player.hidePassword();
+            res.send(player);
+        })
+        .catch ((error) => {
+            console.log(error);
 
-			switch (error.code) {
-				case "VALIDATION_ERROR":
-					res.status(412).send(JSON.stringify({errorCode: 412, erorrMessage: error.message}));
-				default:
-					return res.status(500).send(JSON.stringify({errorCode: 500, erorrMessage: "Something went wrong"}));
-			}
-		});
+            switch (error.code) {
+                case "VALIDATION_ERROR":
+                    res.status(412).send(JSON.stringify({errorCode: 412, erorrMessage: error.message}));
+                default:
+                    return res.status(500).send(JSON.stringify({errorCode: 500, erorrMessage: "Something went wrong"}));
+            }
+        });
 });
 
 app.post("/login", (req, res) => {
-	console.log('Received POST /login');
-	const data = req.body;
+    console.log('Received POST /login');
+    const data = req.body;
 
-	database.findPlayerByUsername(data.username)
-	.then((player) => {
-		return new Promise((resolve, reject) => {
-			if (!player) {
-				const e = new Error('Player not found');
-				e.code = "NOT_FOUND";
-				reject(e);
-			} else {
-				resolve(player);
-			}
-		});
-	})
-	.then((player) => passwordHash.compare(data.password, player.password))
-	.then((isValid) => {
-		if (isValid) {
-			res.status(200).send({message: "OK"});
-		} else {
-			res.status(401).send(JSON.stringify({errorCode: 401, erorrMessage: "Unauthorized"}));
-		}
-	})
-	.catch ((error) => {
-		console.log(error);
+    database.findPlayerByUsername(data.username)
+    .then((player) => {
+        return new Promise((resolve, reject) => {
+            if (!player) {
+                const e = new Error('Player not found');
+                e.code = "NOT_FOUND";
+                reject(e);
+            } else {
+                resolve(player);
+            }
+        });
+    })
+    .then((player) => passwordHash.compare(data.password, player.password))
+    .then((isValid) => {
+        if (isValid) {
+            res.status(200).send({message: "OK"});
+        } else {
+            res.status(401).send(JSON.stringify({errorCode: 401, erorrMessage: "Unauthorized"}));
+        }
+    })
+    .catch ((error) => {
+        console.log(error);
 
-		switch (error.code) {
-			case "NOT_FOUND":
-				return res.status(404).send(JSON.stringify({errorCode: 404, erorrMessage: error.message}));
-			default:
-				return res.status(500).send(JSON.stringify({errorCode: 500, erorrMessage: "Something went wrong"}));
-		}
-	});
+        switch (error.code) {
+            case "NOT_FOUND":
+                return res.status(404).send(JSON.stringify({errorCode: 404, erorrMessage: error.message}));
+            default:
+                return res.status(500).send(JSON.stringify({errorCode: 500, erorrMessage: "Something went wrong"}));
+        }
+    });
 });
 
 // =======
@@ -95,7 +95,7 @@ const server = app.listen(APP_PORT);
 
 /*
 const peerServer = ExpressPeerServer(server, {
-	path: "/myapp",
+    path: "/myapp",
 });
 
 app.use("/peerjs", peerServer);
